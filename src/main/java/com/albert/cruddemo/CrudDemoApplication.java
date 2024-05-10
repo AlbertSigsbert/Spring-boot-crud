e56fd29ab3d18f99e4c7fa026b469a7891d6dbd9
ALBERT <ishengomaalbert@gmail.com>
2024-05-10 12:35:39 +0300

    Saving items to DB

--- src/main/java/com/albert/cruddemo/CrudDemoApplication.java
+++ src/main/java/com/albert/cruddemo/CrudDemoApplication.java
@@ -1,5 +1,7 @@
 package com.albert.cruddemo;
 
+import com.albert.cruddemo.dao.StudentDAO;
+import com.albert.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,23 @@ public class CrudDemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String [] args){
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner -> {
-			System.out.println("Hello world!");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		//create the student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Paul", "Doe", "pauldoe@mail.com");
+
+		//save the student object
+		System.out.println("Saving the student....");
+		studentDAO.save(tempStudent);
+
+		//display id of the saved student
+		System.out.println(STR."Generated ID of the saved Student:\{tempStudent.getId()}");
 	}
 
 }
